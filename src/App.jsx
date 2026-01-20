35d1a49c5b179a2b4577f40684ff7cbdc4f2dff9
ViniSoaresDEV <148481719+ViniSoaresDEV@users.noreply.github.com>
2026-01-20 14:45:08 -0300

    Navigating between screens
    Added functionality for navigating between screens by clicking on options within the side menu.

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ function App() {
   const [editNomeProduto, setEditNomeProduto] = useState("");
   const [editQuantidade, setEditQuantidade] = useState("");
   const [editPreco, setEditPreco] = useState("");
+  const [paginaAtual, setPaginaAtual] = useState("estoque");
 
   // Effect para salvar produtos
   useEffect(() => {
@@ -139,204 +140,206 @@ function App() {
         display: "flex",
       }}
     >
-      <SideMenu onLogout={handleLogout} />
-      <div
-        style={{
-          padding: "20px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "1200px",
-          margin: "0 auto",
-          position: "relative",
-        }}
-      >
-        <h1 className="invetary-title">Controle de Estoque</h1>
-
-        <form
-          onSubmit={adicionarProduto}
+      <SideMenu onLogout={handleLogout} navegar={setPaginaAtual} />
+      {paginaAtual === "estoque" && (
+        <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            marginTop: "20px",
-            gap: "10px",
-            backgroundColor: "white",
-            width: "100%",
             padding: "20px",
-            boxShadow: "3px 6px 8px rgba(0,0,0,0.2)",
-            borderRadius: "15px",
-          }}
-        >
-          <div className="header">
-            <h3>Novo Produto</h3>
-            <span>
-              Valor total em estoque:{" "}
-              <strong>
-                {" "}
-                {valorTotal.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </strong>
-            </span>
-          </div>
-          <div className="box">
-            <div className="input-box">
-              <p>Nome:</p>
-              <input
-                type="text"
-                onChange={(e) => setNomeProduto(e.target.value)}
-                value={nomeProduto}
-              />
-            </div>
-            <div className="input-box">
-              <p>Quantidade:</p>
-              <input
-                type="number"
-                onChange={(e) => setQuantidade(e.target.value)}
-                value={quantidade}
-              />
-            </div>
-            <div className="input-box">
-              <p>Valor:</p>
-              <input
-                type="number"
-                onChange={(e) => setPrecoProduto(e.target.value)}
-                value={precoProduto}
-              />
-            </div>
-          </div>
-          <button type="submit" className="add-btn">
-            Adicionar
-          </button>
-        </form>
-        <ul
-          style={{
             display: "flex",
             flexDirection: "column",
-            gap: "10px",
-            marginTop: "30px",
-            width: "100%",
-            maxHeight: "400px",
-            overflowY: "scroll",
-            paddingRight: "20px",
+            alignItems: "center",
+            width: "1200px",
+            margin: "0 auto",
+            position: "relative",
           }}
         >
-          {produtos.map((produto) => (
-            <li key={produto.id} className="list-box">
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-evenly",
-                  width: "150px",
-                  height: "70px",
-                  fontSize: "20px",
-                }}
-              >
-                <span>Nome</span>
-                <span>{produto.nome}</span>
-              </div>
+          <h1 className="invetary-title">Controle de Estoque</h1>
 
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-evenly",
-                  width: "150px",
-                  height: "70px",
-                  fontSize: "20px",
-                }}
-              >
-                <span>Quantidade</span>
-                <span>{produto.quantidade}</span>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-evenly",
-                  width: "150px",
-                  height: "70px",
-                  fontSize: "20px",
-                }}
-              >
-                <span>Preço</span>
-                <span>
-                  {produto.preco.toLocaleString("pt-BR", {
+          <form
+            onSubmit={adicionarProduto}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginTop: "20px",
+              gap: "10px",
+              backgroundColor: "white",
+              width: "100%",
+              padding: "20px",
+              boxShadow: "3px 6px 8px rgba(0,0,0,0.2)",
+              borderRadius: "15px",
+            }}
+          >
+            <div className="header">
+              <h3>Novo Produto</h3>
+              <span>
+                Valor total em estoque:{" "}
+                <strong>
+                  {" "}
+                  {valorTotal.toLocaleString("pt-BR", {
                     style: "currency",
                     currency: "BRL",
                   })}
-                </span>
+                </strong>
+              </span>
+            </div>
+            <div className="box">
+              <div className="input-box">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  onChange={(e) => setNomeProduto(e.target.value)}
+                  value={nomeProduto}
+                />
               </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-evenly",
-                  width: "150px",
-                  height: "70px",
-                  fontSize: "20px",
-                }}
-              >
-                <span>Valor total</span>
-                <span>
-                  {(
-                    Number(produto.quantidade) * Number(produto.preco)
-                  ).toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  })}
-                </span>
+              <div className="input-box">
+                <p>Quantidade:</p>
+                <input
+                  type="number"
+                  onChange={(e) => setQuantidade(e.target.value)}
+                  value={quantidade}
+                />
               </div>
-              <button
-                style={{
-                  marginLeft: "10px",
-                  textAlign: "center",
-                  backgroundColor: "transparent",
-                  border: "none",
-                  cursor: "pointer",
-                  color: "red",
-                  fontSize: "18px",
-                  width: "100px",
-                }}
-                onClick={() => removerProduto(produto.id)}
-              >
-                <FaRegTrashAlt /> Remover
-              </button>
-              <button
-                style={{
-                  marginLeft: "10px",
-                  textAlign: "center",
-                  backgroundColor: "transparent",
-                  border: "none",
-                  cursor: "pointer",
-                  color: "#507CFF",
-                  fontSize: "18px",
-                  width: "100px",
-                }}
-                onClick={() => preparaEdicao(produto)}
-              >
-                <FaRegEdit /> Editar
-              </button>
-            </li>
-          ))}
-        </ul>
+              <div className="input-box">
+                <p>Valor:</p>
+                <input
+                  type="number"
+                  onChange={(e) => setPrecoProduto(e.target.value)}
+                  value={precoProduto}
+                />
+              </div>
+            </div>
+            <button type="submit" className="add-btn">
+              Adicionar
+            </button>
+          </form>
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              marginTop: "30px",
+              width: "100%",
+              maxHeight: "400px",
+              overflowY: "scroll",
+              paddingRight: "20px",
+            }}
+          >
+            {produtos.map((produto) => (
+              <li key={produto.id} className="list-box">
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    width: "150px",
+                    height: "70px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <span>Nome</span>
+                  <span>{produto.nome}</span>
+                </div>
 
-        {/*MODAL SECTION*/}
-        {modalEdit && (
-          <ModalEditar
-            fecharModal={() => setModalEdit(!modalEdit)}
-            salvar={salvaEdicao}
-            nome={editNomeProduto}
-            setNome={setEditNomeProduto}
-            qtd={editQuantidade}
-            setQtd={setEditQuantidade}
-            preco={editPreco}
-            setPreco={setEditPreco}
-          />
-        )}
-      </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    width: "150px",
+                    height: "70px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <span>Quantidade</span>
+                  <span>{produto.quantidade}</span>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    width: "150px",
+                    height: "70px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <span>Preço</span>
+                  <span>
+                    {produto.preco.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    width: "150px",
+                    height: "70px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <span>Valor total</span>
+                  <span>
+                    {(
+                      Number(produto.quantidade) * Number(produto.preco)
+                    ).toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+                </div>
+                <button
+                  style={{
+                    marginLeft: "10px",
+                    textAlign: "center",
+                    backgroundColor: "transparent",
+                    border: "none",
+                    cursor: "pointer",
+                    color: "red",
+                    fontSize: "18px",
+                    width: "100px",
+                  }}
+                  onClick={() => removerProduto(produto.id)}
+                >
+                  <FaRegTrashAlt /> Remover
+                </button>
+                <button
+                  style={{
+                    marginLeft: "10px",
+                    textAlign: "center",
+                    backgroundColor: "transparent",
+                    border: "none",
+                    cursor: "pointer",
+                    color: "#507CFF",
+                    fontSize: "18px",
+                    width: "100px",
+                  }}
+                  onClick={() => preparaEdicao(produto)}
+                >
+                  <FaRegEdit /> Editar
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          {/*MODAL SECTION*/}
+          {modalEdit && (
+            <ModalEditar
+              fecharModal={() => setModalEdit(!modalEdit)}
+              salvar={salvaEdicao}
+              nome={editNomeProduto}
+              setNome={setEditNomeProduto}
+              qtd={editQuantidade}
+              setQtd={setEditQuantidade}
+              preco={editPreco}
+              setPreco={setEditPreco}
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Side Menu/SideMenu.jsx
+++ src/components/Side Menu/SideMenu.jsx
@@ -3,19 +3,19 @@ import { MdOutlinePointOfSale } from "react-icons/md";
 import { BsBoxSeam } from "react-icons/bs";
 import { MdLogout } from "react-icons/md";
 
-function SideMenu({ onLogout }) {
+function SideMenu({ onLogout, navegar }) {
   return (
     <aside className="side-menu">
       <nav>
         <h2 style={{ marginBottom: "30px", color: "white" }}>Bellano Móveis</h2>
         <ul className="sidebar-list">
           <li>
-            <a href="#">
+            <a onClick={() => navegar("estoque")}>
               <BsBoxSeam /> Gestor de Estoque
             </a>
           </li>
           <li>
-            <a href="#">
+            <a onClick={() => navegar("orcamento")}>
               <MdOutlinePointOfSale /> Orçamentos
             </a>
           </li>
