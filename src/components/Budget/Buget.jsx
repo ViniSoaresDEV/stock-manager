9b8d6eef7473442914a550fe0775141679f5153f
ViniSoaresDEV <148481719+ViniSoaresDEV@users.noreply.github.com>
2026-01-29 14:31:34 -0300

    Logic and CSS in the Budget component
    New fields and logic were added to display the searched items in the budget list, and the calculation logic was also implemented.

--- src/components/Budget/Buget.jsx
+++ src/components/Budget/Buget.jsx
@@ -1,30 +1,49 @@
 import './Budget.css';
 import { useState } from 'react';
+import { FaRegTrashAlt, FaTrashAlt } from 'react-icons/fa';
 
 function Budget() {
   const produtosDisponiveis = [
-    { id: 1, nome: 'Mesa de Jantar', preco: 850.0 },
+    { id: 1, nome: 'Mesa de Jantar 6 Lugares', preco: 850.0 },
     { id: 2, nome: 'Cadeira Style', preco: 150.0 },
     { id: 3, nome: 'Armário Planejado', preco: 1200.0 },
-    { id: 4, nome: 'Sofá 3 Lugares', preco: 2000.0 },
+    { id: 4, nome: 'Sofá Retrátil', preco: 2000.0 },
+    { id: 5, nome: 'Rack para TV', preco: 450.0 },
+    { id: 6, nome: 'Bancada Profissional Dupla Face 4 Estações', preco: 450.0 },
+    { id: 7, nome: 'Armário Auxiliar de Lavatório Inferior', preco: 450.0 },
+    { id: 8, nome: 'Cadeira New Roma', preco: 450.0 },
+    { id: 9, nome: 'Lavatório New Roma', preco: 450.0 },
   ];
 
   const [gerarOrcamentoBtn, setGerarOrcamentoBtn] = useState('');
 
   const [termoBuscado, setTermoBuscado] = useState('');
-  const [itensOrcamento, setItemOrcamento] = useState('');
+  const [itensOrcamento, setItemOrcamento] = useState([]);
+  const [desconto, setDesconto] = useState(0);
+  const [frete, setFrete] = useState(0);
+
+  const valorTotal = Array.isArray(itensOrcamento)
+    ? itensOrcamento.reduce((acc, item) => {
+        return acc + item.preco * item.quantidade;
+      }, 0)
+    : 0;
+
+  const calculoDesconto = (valorTotal / 100) * desconto;
+
+  const valorTotalFinal = valorTotal - calculoDesconto + Number(frete);
 
   function buscarEAdicionar() {
     if (!termoBuscado) return alert('Por favor, digite o nome do produto.');
 
-    const termoEncontrado = produtosDisponiveis.filter((produto) =>
+    const termoEncontrado = produtosDisponiveis.find((produto) =>
       produto.nome
         .toLocaleLowerCase()
         .includes(termoBuscado.toLocaleLowerCase()),
     );
+    console.log(termoEncontrado);
 
     if (termoEncontrado) {
-      adicionarAoOrcamento(termoBuscado);
+      adicionarAoOrcamento(termoEncontrado);
     } else {
       alert('Item não encontrado!');
     }
@@ -57,6 +76,7 @@ function Budget() {
   }
 
   function removeItem(id) {
+    if (!confirm('Tem certeza que deseja remover o item?')) return;
     setItemOrcamento(itensOrcamento.filter((item) => item.id !== id));
   }
 
@@ -82,13 +102,13 @@ function Budget() {
                   <span>Nome Completo: </span> <input type="text" />
                 </div>
                 <div className="dados-cliente-input">
-                  <span>CPF / CNPJ: </span> <input type="text" />
+                  <span>CPF / CNPJ: </span> <input type="number" />
                 </div>
                 <div className="dados-cliente-input">
-                  <span>E-mail: </span> <input type="text" />
+                  <span>E-mail: </span> <input type="email" />
                 </div>
                 <div className="dados-cliente-input">
-                  <span>Telefone: </span> <input type="text" />
+                  <span>Telefone: </span> <input type="tel" />
                 </div>
                 <div className="dados-cliente-input">
                   <span>Endereço: </span> <input type="text" />
@@ -96,6 +116,7 @@ function Budget() {
                 <div className="dados-cliente-input">
                   <span>Vendedor: </span>{' '}
                   <select id="select-vendedor">
+                    <option value="0">Selecionar vendedor</option>
                     <option value="1">Raquel</option>
                     <option value="2">Thays</option>
                     <option value="3">Aldeir</option>
@@ -111,7 +132,6 @@ function Budget() {
                     <option value="1">Dinheiro</option>
                   </select>
                 </div>
-                <button>Gerar PDF do Orçamento</button>
               </div>
             </div>
             <div className="buscar-produto-box">
@@ -126,17 +146,128 @@ function Budget() {
                 <button onClick={buscarEAdicionar}>Adicionar</button>
               </div>
               <div className="buscar-produto-lista">
-                <tr>
-                  <th>Produto</th>
-                  <th>Quantidade</th>
-                  <th>Valor Unitário</th>
-                  <th>Observação</th>
-                </tr>
-                <tr id="tr-itens">
-                  <td></td>
-                </tr>
+                {itensOrcamento.length > 0 ? (
+                  <div className="lista-produto">
+                    <table
+                      style={{ width: '100%', borderCollapse: 'collapse' }}
+                    >
+                      <thead>
+                        <tr
+                          style={{
+                            borderBottom: '2px solid #eee',
+                            textAlign: 'left',
+                            color: '#555',
+                          }}
+                        >
+                          <th style={{ padding: '10px' }}>Produto</th>
+                          <th style={{ padding: '10px' }}>Qtd</th>
+                          <th style={{ padding: '10px' }}>Valor Un</th>
+                          <th style={{ padding: '10px' }}>Observação</th>
+                          <th style={{ padding: '10px' }}>Ação</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {itensOrcamento.map((item) => (
+                          <tr
+                            key={item.id}
+                            style={{ borderBottom: '1px solid #eee' }}
+                          >
+                            <td>{item.nome}</td>
+                            <td>
+                              <input
+                                style={{ width: '40px' }}
+                                type="number"
+                                min={1}
+                                value={item.quantidade}
+                                onChange={(e) =>
+                                  atualizarValor(
+                                    item.id,
+                                    'quantidade',
+                                    e.target.value,
+                                  )
+                                }
+                              />
+                            </td>
+                            <td style={{ paddingLeft: '5px' }}>
+                              {(item.preco * item.quantidade).toLocaleString(
+                                'pt-BR',
+                                { style: 'currency', currency: 'BRL' },
+                              )}
+                            </td>
+                            <td>
+                              <textarea
+                                onChange={(e) =>
+                                  atualizarValor(
+                                    item.id,
+                                    'observacao',
+                                    e.target.value,
+                                  )
+                                }
+                                placeholder="Obs..."
+                                value={item.observacao}
+                              ></textarea>
+                            </td>
+                            <td>
+                              <button
+                                className="remove-btn"
+                                onClick={() => removeItem(item.id)}
+                              >
+                                <FaTrashAlt /> Remover
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                ) : (
+                  'Nenhum item adicionado'
+                )}
+              </div>
+              <div className="valor-total-box">
+                <div className="valor-total-input">
+                  <div>
+                    <span>Desconto (%): </span>
+                    <input
+                      type="number"
+                      onChange={(e) => setDesconto(e.target.value)}
+                    />
+                  </div>
+                  <div>
+                    <span>Valor do Frete: </span>
+                    <input
+                      type="number"
+                      onChange={(e) => setFrete(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="valor-total-resultado">
+                  <span>
+                    Total:{' '}
+                    {valorTotal.toLocaleString('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                  </span>
+                  <span>Desconto: {desconto + '%'}</span>
+                  <span>
+                    Frete:{' '}
+                    {Number(frete).toLocaleString('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                  </span>
+                  <span>
+                    Total Final:{' '}
+                    {valorTotalFinal.toLocaleString('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                  </span>
+                </div>
               </div>
             </div>
+            <button className="pdf-btn">Gerar PDF do Orçamento</button>
           </div>
         </>
       )}
